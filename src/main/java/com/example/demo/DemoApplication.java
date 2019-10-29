3014d63dd949e47fd851d2cf4420b97ebf0722d3
Ovidio Molina <ocmolina@users.noreply.github.com>
2019-10-29 15:43:23 -0600

    hello (#5)
    * hello
    
    * hello again

--- src/main/java/com/example/demo/DemoApplication.java
+++ src/main/java/com/example/demo/DemoApplication.java
@@ -7,6 +7,8 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 public class DemoApplication {
 
 	public static void main(String[] args) {
+		System.out.println("hello");
+		System.out.println("hello there again!");
 		SpringApplication.run(DemoApplication.class, args);
 	}
 
