87c1d546c7576c387acaa45e1fc098223e8bb2bd
Ovidio Molina <ovidio.molina@ipointsystems.com>
2019-10-30 11:27:10 -0600

    change

--- src/main/java/com/example/demo/DemoApplication.java
+++ src/main/java/com/example/demo/DemoApplication.java
@@ -1,10 +1,22 @@
 package com.example.demo;
 
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
 
 @SpringBootApplication
+@RestController
 public class DemoApplication {
+
+    @Value("${server.port}")
+    private String port;
+
+    @GetMapping(value = "/hello")
+	public String hello() {
+	    return "hello there! I'm running on port " + port;
+    }
 
 	public static void main(String[] args) {
 		System.out.println("test");
